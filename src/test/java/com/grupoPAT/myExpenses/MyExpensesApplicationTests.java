dc4860df1f68ca93507a628a6ba5f3bf3f058279
inessaavedra <ines.saavedra1@gmail.com>
2022-04-14 16:05:02 +0200

    m

---
+++ src/test/java/com/grupoPAT/myExpenses/MyExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoPAT.myExpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
